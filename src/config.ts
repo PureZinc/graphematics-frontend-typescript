a95314ceb2f2f70ae3bd904dc2205d356279ef0c
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-11-15 13:07:26 -0500

    Now uses the right Backend

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:8000/api"; //Testing
-// export const BASE_URL = "https://graphematics-26e0b7201e5f.herokuapp.com/api";
+// export const BASE_URL = "http://127.0.0.1:8000/api"; //Testing
+export const BASE_URL = "https://graphematics-26e0b7201e5f.herokuapp.com/api";
